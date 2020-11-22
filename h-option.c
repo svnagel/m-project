582c3e755c98bb32b1ec4a70c6f05d08c4fdbefd
svnagel <35927431+svnagel@users.noreply.github.com>
2020-11-22 21:35:58 +0000

    Update h-option.c

--- h-option.c
+++ h-option.c
@@ -1,5 +1,6 @@
 #https://stackoverflow.com/questions/498320/pass-arguments-into-c-program-from-command-line
 #https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
+#int main(int argc, char **argv)
 
 char *hvalue = "Welcome to help menu...";
 int opt = 0;
